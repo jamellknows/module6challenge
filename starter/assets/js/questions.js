acec2a844d8467c23144970cbc2885d320a7fd3f
jamellknows <jamellsamuels@googlemail.com>
2023-01-14 23:09:38 +0000

    Added new files and made an alert box

---
+++ starter/assets/js/questions.js
@@ -0,0 +1,11 @@
+let questionsEl = document.getElementById("questions")
+
+let questionTitleEl = document.getElementById("question-title")
+
+let choicesEl = document.getElementById("choices")
+
+
+function questions()
+{
+    alert("Hello")
+}
