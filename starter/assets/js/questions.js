1844c09c21010b1acaaf4ab79d0947ce1417c715
jamellknows <jamellsamuels@googlemail.com>
2023-01-15 15:27:38 +0000

    Rendered questions on the page and started to write questions

--- starter/assets/js/questions.js
+++ starter/assets/js/questions.js
@@ -4,8 +4,74 @@ let questionTitleEl = document.getElementById("question-title")
 
 let choicesEl = document.getElementById("choices")
 
+let startEl = document.getElementById("start-screen")
+let questionNumber = 1;
+
+const questionsObj = {
+    1:{"What is the HTML tag used for javascript?" : ["<script></script>", "<javascript></javascript>", "<function></function>","<source></source>"]},
+    2:{"What is the source tag used for?" : ["An object", "Videos", "All MultiMedia", "Images"]},
+    3:{"What is the correct way to select a child element inside a parent element in css":["parent child", ""]},
+    4:{"How do you write media queries":[]},
+    4:{"What order should media queries be written if going from smallest to biggest?":[]},
+    4:{"":[]},
+    4:{"":[]},
+    4:{"":[]},
+    4:{"":[]},
+    4:{"":[]},
+    4:{"":[]},
+    4:{"":[]},
+    4:{"":[]},
+    4:{"":[]},
+    4:{"":[]},
+    4:{"":[]},
+    4:{"":[]},
+    4:{"":[]},
+    4:{"":[]},
+    4:{"":[]},
+
+}
+    
 
 function questions()
 {
-    alert("Hello")
+    startEl.style.display = "none"
+    questionsEl.classList.remove("hide")
+    renderQuestions()
+    
 }
+
+function renderQuestions()
+{
+    const nextQuestionEl = document.createElement("button")
+    const prevQuestionEl = document.createElement("button")
+    nextQuestionEl.innerText = "Next Question"
+    prevQuestionEl.innerText = "Previous Question"
+    nextQuestionEl.classList.add("start")
+    prevQuestionEl.classList.add("start")
+    nextQuestionEl.classList.add("start")
+    prevQuestionEl.classList.add("start")
+    questionsEl.appendChild(nextQuestionEl)
+    questionsEl.appendChild(prevQuestionEl)
+
+
+    nextQuestionEl.addEventListener('click', function()
+    {
+    if(questionNumber < questionsObj.length)
+        {
+        ++questionNumber
+        console.log(questionNumber)
+        }
+    
+    })
+
+prevQuestionEl.addEventListener('click', function(){
+    if(questionNumber > 0)
+    {
+        --questionNumber
+    }
+    })
+
+    questionTitleEl.textContent = `${Object.keys(questionsObj[questionNumber])}`
+
+}
+
