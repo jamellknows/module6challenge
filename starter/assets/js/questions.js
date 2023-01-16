8f9188bfe3cd71086a5adbd20a0ab7d9a7d589a7
jamellknows <jamellsamuels@googlemail.com>
2023-01-16 17:33:49 +0000

    added a score function, finished the questions, updated the buttons

--- starter/assets/js/questions.js
+++ starter/assets/js/questions.js
@@ -6,38 +6,49 @@ let choicesEl = document.getElementById("choices")
 
 let startEl = document.getElementById("start-screen")
 let questionNumber = 1;
-const nextQuestionEl = document.createElement("button")
+const buttonsEl = document.createElement("div")
 const prevQuestionEl = document.createElement("button")
+const nextQuestionEl = document.createElement("button")
+
+let timeEl = document.getElementById("time")
+let time = 0;
+let score = 0
+
+
 const questionsObj = {
-    1:{"What is the HTML tag used for javascript?" : ["<script></script>", "<javascript></javascript>", "<function></function>","<source></source>"]},
+    1:{"What is the HTML tag <> used for writing JavaScript?" : ["script", "javascript", "function","source"]},
     2:{"What is the source tag used for?" : ["An object", "Videos", "All MultiMedia", "Images"]},
-    3:{"What is the correct way to select a child element inside a parent element in css":["parent child", "child parent", "parent > child ", "child < parent"]},
-    4:{"How do you write css media queries":["@media screen", "@query screen", "@repsonsive screen", "@screen"]},
-    5:{"What order should media queries be written if writing a max width/ height media queries ":[]},
-    6:{"Why is it best practice to use semantic elements?":["To make it easier to see the function while coding", "To make it easier to write javascript code for", "To ensure that the webpage is accessible for all users (blind/partially sighted/deaf etc)", ""]},
-    7:{"What method converts a Javascript Object to a String":["object.toString()", "Object.toString(object)", "JSON.stringify(object)", "stringify(object)"]},
-    8:{"How do you write an event listener in Javascript?":[]},
-    9:{"What attribute is not an event for an Event Listener in Javascript":[]},
-    10:{"Where do you insert the link tag for bootstrap":[]},
-    11:{"How do you display all the keys in a Javascript Object?":[]},
-    12:{"How do ":[]},
-    13:{"":[]},
-    14:{"":[]},
-    15:{"":[]},
-    16:{"":[]},
-    17:{"":[]},
-    18:{"":[]},
-    19:{"":[]},
-    20:{"":[]},
+    3:{"What is the correct way to select a child element inside of a parent element in css":["parent child", "child parent", "parent > child ", "child < parent"]},
+    4:{"How do you write css media queries":["@media screen", "@query screen", "@responsive screen", "@screen"]},
+    5:{"What order should media queries be written if writing a max width/height media queries ":["top to bottom", "bottom to top", "in any order"]},
+    6:{"Why is it best practice to use semantic elements?":["To make it easier to see the function while coding", "To make it easier to write javascript code", "To ensure that the webpage is accessible for all users (blind/partially sighted/deaf etc)", ""]},
+    7:{"What method converts a JavaScript Object to a String":["object.toString()", "Object.toString(object)", "stringify(object)", "JSON.stringify(object)" ]},
+    8:{"How do you write an event listener in JavaScript?":["function eventListener()", "eventListener(function)", "element.addEventListener()", "addEventListener(element)"]},
+    9:{"What attribute is not an event for an Event Listener in JavaScript":["click", "touch", "submit",  "keypress"]},
+    10:{"Where do you insert the link tag for bootstrap":["head", "body", "meta", "script"]},
+    11:{"How do you display all the keys in a JavaScript Object?":["Object.keysFrom(object)","Object.keys(object)", "keys(object)",  "object.getKeys()"]},
+    12:{"Which of these is not a data type in JavaScript? ":["String", "char", "Number", "BigInt"]},
+    13:{"What is the definition of hoisting in JavaScript?":["Maintaining a server over the cloud", "Moving all variable and function declarations to the top of the scope", "Calling  function inside another function", "Pulling and merging a github source into your code "]},
+    14:{"What is the difference between == and ===?":["== is numerically approximate comparison whereas === is exact", "=== is for Strings == is for Numbers", "There is no difference", "== is used to compare values === is used to compare value and type"]},
+    15:{"What is the difference between the var and let key words?":[" var is global scoped and let is function scoped", "var is function scoped and let is block scoped", "var is block scoped and let is function scoped", "var is global scoped and let is function scoped "]},
+   
 
 }
-    
+
+const answersArr = [1, 3, 3, 1, 1, 3, 4, 3, 2, 3, 3, 2, 2, 4,2]
+
+
 
 function questions()
 {
+
     startEl.style.display = "none"
     questionsEl.classList.remove("hide")
     renderQuestions()
+    var timer = setInterval(function(){
+        time++
+        timeEl.textContent =`${time}`
+    }, 1000)
     
 }
 
@@ -50,24 +61,30 @@ function renderQuestions()
     prevQuestionEl.classList.add("start")
     nextQuestionEl.classList.add("start")
     prevQuestionEl.classList.add("start")
-    questionsEl.appendChild(nextQuestionEl)
-    questionsEl.appendChild(prevQuestionEl)
-    console.log(Object.values(questionsObj[questionNumber])[0][0])
+    buttonsEl.classList.add("center")
+    questionsEl.appendChild(buttonsEl)
+    buttonsEl.appendChild(nextQuestionEl)
+    buttonsEl.appendChild(prevQuestionEl)
     let question1 = JSON.stringify(Object.values(questionsObj[questionNumber])[0][0])
-    console.log(question1)
-
+    let question2 = JSON.stringify(Object.values(questionsObj[questionNumber])[0][1])
+    let question3 = JSON.stringify(Object.values(questionsObj[questionNumber])[0][2])
+    let question4 = JSON.stringify(Object.values(questionsObj[questionNumber])[0][3])
+    Tally()
 
    
 
     questionTitleEl.textContent = `${Object.keys(questionsObj[questionNumber])}`
     choices.innerHTML = 
-    `<form>
+    `
+    
+    
+    <form>
     <label for="answers">Select an answer:</label>
     <select name="answers" id="answers">
     <option value="1">${question1}</option>
-    <option value="2">${Object.values(questionsObj[questionNumber])[0][1]}</option>
-    <option value="3">${Object.values(questionsObj[questionNumber])[0][2]}</option>
-    <option value="4">${Object.values(questionsObj[questionNumber])[0][3]}</option>
+    <option value="2">${question2}</option>
+    <option value="3">${question3}</option>
+    <option value="4">${question4}</option>
     </select>
     </form>`
 
@@ -75,7 +92,7 @@ function renderQuestions()
 
 nextQuestionEl.addEventListener('click', function()
 {
-if(questionNumber < Object.keys(questionsObj).length)
+    if(questionNumber < Object.keys(questionsObj).length)
     {
     ++questionNumber
     renderQuestions()
@@ -84,6 +101,10 @@ if(questionNumber < Object.keys(questionsObj).length)
     else{
         renderQuestions()
     }
+    // on click get value if on last question submit button 
+    // check if right answer store value summate
+
+
 
 })
 
@@ -99,4 +120,19 @@ else{
     
 }
 })
+
+function Tally()
+{
+    let e = document.getElementById("answers")
+    if(e === null){
+        return 
+    }
+    let option = e.value 
+    if(answersArr[questionNumber] == option)
+    {
+        score ++
+        console.log(score)
+    }
+    // check if right score against the answer 
+}
 
